2aafd3a686818d6f003f3d53b7dd0b8cbc581c89
faizhermawan_ <faizhermawan13@gmail.com>
2022-06-14 00:11:20 +0700

    add template admin-kit as front-end

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Dashboard extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +18,11 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data['content'] = 'pages/v_dashboard';
+		$this->load->view('layouts/v_layout', $data);
 	}
+	
 }
---
+++ application/views/layouts/v_layout.php
@@ -0,0 +1,5 @@
+<?php 
+	$this->load->view('layouts/v_header');
+	$this->load->view($content);
+	$this->load->view('layouts/v_footer');
+?>
